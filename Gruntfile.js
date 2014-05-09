cd49dff84c9e11e9ded1513ecdcaaa3a53aee792
Caleb Winters <winters.caleb@gmail.com>
2014-05-09 11:11:13 -0700

    initial commit, add assemble. boom

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,26 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    assemble: {
+      options: {
+        flatten: false,
+        assets: 'dist/assets/',
+        data: ['src/data/*.json'],
+      },
+      dist: {
+        options: {
+          partials: ['src/includes/*.html'],
+          helpers: ['src/helpers/*.js'],
+          layout: 'src/layouts/default.html'
+        },
+        expand: true,
+        cwd: 'src/pages/',
+        src: '**/*.html',
+        dest: 'dist/'
+      }
+    },
 
     sass: {
       options: {
@@ -8,27 +28,78 @@ module.exports = function(grunt) {
       },
       dist: {
         options: {
-          outputStyle: 'compressed'
+          outputStyle: 'expanded'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'dist/assets/css/app.css': 'src/assets/scss/app.scss'
         }        
       }
     },
 
+    copy: {
+      dist: {
+        files: [
+          {expand:true, cwd: 'src/assets/', src: ['**/*','!{scss,js}/**/*'], dest: 'dist/assets/', filter:'isFile'},
+          {expand:true, cwd: 'bower_components/modernizr/', src: 'modernizr.js', dest: 'dist/assets/js', filter:'isFile'}
+        ]
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/assets/js/all.js': ['bower_components/jquery/dist/jquery.js', 'bower_components/foundation/js/foundation.js', 'src/assets/js/*']
+        }
+      }
+    },
+
+    clean: ['dist/'],
+
     watch: {
-      grunt: { files: ['Gruntfile.js'] },
+      grunt: { 
+        files: ['Gruntfile.js'],
+        tasks: ['build'] 
+      },
 
       sass: {
-        files: 'scss/**/*.scss',
+        files: 'src/assets/scss/**/*.scss',
         tasks: ['sass']
+      },
+
+      copy: {
+        options: {cwd: 'src/assets/'},
+        files: ['**/*','!{scss,js}/**/*'],
+        tasks: ['copy']
+      },
+
+      uglify: {
+        options: {cwd: 'src/assets/js'},
+        files: ['**/*.js'],
+        tasks: ['uglify']
+      },
+
+      assemble_all: {
+        files: ['src/{includes,layouts}/**/*.html'],
+        tasks: ['assemble'],
+        options: {livereload:true}
+      },
+
+      assemble_pages: {
+        files: ['src/pages/**/*.html'],
+        tasks: ['newer:assemble'],
+        options: {livereload:true}
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-newer');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['clean','sass','uglify','assemble','copy']);
   grunt.registerTask('default', ['build','watch']);
 }
